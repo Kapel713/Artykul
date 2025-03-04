f46bd479cbfe792a4eb7556eb303399eeebc82c7
Kapel713 <anniversarybully96@gmail.com>
2025-03-04 22:45:22 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const button = document.querySelector(".button");
-    button.addEventListener("mouseover", () => {
-        button.style.backgroundColor = "#9333ea";
-    });
-    button.addEventListener("mouseout", () => {
-        button.style.backgroundColor = "#a855f7";
-    });
-});
